eb28c16b774980224ee7c1618ed2f5ba647b905f
Rameshkartik <rameshkartik>
2017-05-29 21:55:26 +0530

    First Commit

---
+++ LinqToSql/LinqToSql/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LinqToSql.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+    </div>
+        <asp:Button ID="btn_Get" runat="server" OnClick="btn_Get_Click" Text="Get Record" />
+        <asp:Button ID="btn_Insert" runat="server" OnClick="btn_Insert_Click" Text="Insert Record" />
+        <asp:Button ID="Bn_Upd" runat="server" Text="Update Record" />
+        <asp:Button ID="btn_Del" runat="server" Text="Delete Record " />
+    </form>
+</body>
+</html>
---
+++ LinqToSql/LinqToSql/WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqToSql
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetMethod();
+        }
+
+        private void GetMethod()
+        {
+            DataClasses1DataContext Dbcntxt = new DataClasses1DataContext();
+            GridView1.DataSource = from emp in Dbcntxt.Employees
+                                   where emp.Job == "Clerk"
+                                   orderby (emp.EmpNo) ascending
+                                   select emp;
+
+
+            GridView1.DataBind();
+        }
+
+        protected void btn_Get_Click(object sender, EventArgs e)
+        {
+            GetMethod();
+        }
+
+        protected void btn_Insert_Click(object sender, EventArgs e)
+        {
+            using (DataClasses1DataContext dcnt = new DataClasses1DataContext())
+            {
+                Employee emp = new Employee()
+                {
+                    EmpNo = 101,
+                    EmpName = "Rameshkartik",
+                    Comm = 1500,
+                    DEPTNO = 10,
+                    Hiredate = Convert.ToDateTime("1980-12-17"),
+                    Job = "Clerk",
+                    Mgr = 10,
+                    Sal = 15000
+                };
+                dcnt.Employees.InsertOnSubmit(emp);
+                dcnt.SubmitChanges();
+                
+            }
+            GetMethod();
+        }
+
+
+
+        
+    }
+}
